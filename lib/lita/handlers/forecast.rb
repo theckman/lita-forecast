716df26f5a90a0ed78d8cf8e6e5529d2e53b6f56
Tim Heckman <tim@timheckman.net>
2014-03-16 21:03:07 -0700

    remove requires from forecast handler as it is a no-op

--- lib/lita/handlers/forecast.rb
+++ lib/lita/handlers/forecast.rb
@@ -1,18 +1,17 @@
 # -*- coding: UTF-8 -*-
-require 'forecast_io'
-require 'geocoder'
-require 'compass_rose'
-require 'lita/handlers/forecast/location'
-
+# This is a dummy Handler so the forecast.io API key has a reasonable
+# namespace
+#
 module Lita
   module Handlers
     # Lita Forecast.io plugin!
+    # This Handler is basically a no-op and is used only for the
+    # 'forecast' handler namespace.
     #
     class Forecast < Handler
       def self.default_config(config)
         config.api_key = nil
       end
-
       Lita.register_handler(Forecast)
     end
   end
