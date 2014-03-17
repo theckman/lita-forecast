95ebc084df9436e1ec1041d65240ce882a2fac51
Tim Heckman <tim@timheckman.net>
2014-03-16 21:30:24 -0700

    still remove the note to myself

--- lib/lita-forecast.rb
+++ lib/lita-forecast.rb
@@ -5,6 +5,3 @@ require 'lita/handlers/forecast'
 require 'lita/handlers/forecast_us'
 require 'lita/handlers/forecast_locations'
 require 'lita/handlers/location_search'
-
-# YOU NEED TO UNIFY THE HANDLERS (fuck) AND MAKE EACH THING ITS OWN CLASS
-# I GUESS...
