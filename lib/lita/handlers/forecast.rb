e8243bab06b99ed94f637e0a74f07cd11253598f
Tim Heckman <tim@timheckman.net>
2014-03-23 18:55:33 -0700

    improve CodeClimate score by unifying the handlers

--- lib/lita/handlers/forecast.rb
+++ lib/lita/handlers/forecast.rb
@@ -1,17 +1,56 @@
 # -*- coding: UTF-8 -*-
-# This is a dummy Handler so the forecast.io API key has a reasonable
-# namespace
-#
+require 'lita/handlers/forecast/mixins'
+require 'lita/handlers/forecast/weather'
+
 module Lita
   module Handlers
     # Lita Forecast.io plugin!
-    # This Handler is basically a no-op and is used only for the
-    # 'forecast' handler namespace.
+    # this provides handlers for US and Canadian units
     #
     class Forecast < Handler
+      include LitaForecast::Mixins
+      include LitaForecast::Weather
+
       def self.default_config(config)
         config.api_key = nil
       end
+
+      route(
+        /^wx\s(.*)$/,
+        :weather_us,
+        command: true,
+        help: {
+          'wx san francisco' => 'Get the weather for San Francisco'
+        }
+      )
+
+      route(
+        /^wc\s(.*)$/,
+        :weather_ca,
+        command: true,
+        help: {
+          'wc toronto' => 'You want the weather for Toronto, eh?'
+        }
+      )
+
+      def weather_us(response)
+        response.reply(build_weather('us', response))
+      end
+
+      def weather_ca(response)
+        response.reply(build_weather('ca', response))
+      end
+
+      def build_weather(units, response)
+        weather(response, api_key, units)
+      end
+
+      private
+
+      def api_key
+        Lita.config.handlers.forecast.api_key
+      end
+
       Lita.register_handler(Forecast)
     end
   end
